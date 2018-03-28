974e5ef3b663d3fb678bdf7824e1c320aa71f0bb
Ashley <ashleyk9@hotmail.com>
2018-03-28 09:49:52 +0200

    Update CollisionAvoidance stoppen

--- C11-B/src/nl/hva/miw/robot/cohort11/CollisionAvoidance.java
+++ C11-B/src/nl/hva/miw/robot/cohort11/CollisionAvoidance.java
@@ -14,75 +14,80 @@ import lejos.utility.Delay;
 
 public class CollisionAvoidance {
 	public static void main(String[] args) {
-	EV3IRSensor infraRed = new EV3IRSensor(SensorPort.S2); //tijdelijk op port 2
-	EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
-	EV3LargeRegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
-	final int NINETY_DEGREES = 90;
-	final int DEFAULT_MOTOR_SPEED = 100;
-	SampleProvider rangeSampler;
-	float[] lastRange;
-	
-	System.out.println("Drive and Avoid\n");
-    System.out.println("Press any key to start");
-    
-    Button.LEDPattern(4);    // flash green led and 
-    Sound.beepSequenceUp();  // make sound when ready.
+		EV3IRSensor infraRed = new EV3IRSensor(SensorPort.S2); // tijdelijk op port 2
+		EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
+		final int NINETY_DEGREES = 90;
+		final int DEFAULT_MOTOR_SPEED = 100;
+		SampleProvider rangeSampler;
+		float[] lastRange;
 
-    Button.waitForAnyPress();
-	
-	motorB.setSpeed(DEFAULT_MOTOR_SPEED);
-	motorC.setSpeed(DEFAULT_MOTOR_SPEED);
-	motorB.forward();
-	motorC.forward();
-	Delay.msDelay(3000);
-	
-	rangeSampler = infraRed.getDistanceMode();
-	int distanceValue = 0;
+		System.out.println("Drive and Avoid\n");
+		System.out.println("Press any key to start");
 
-    final int iteration_threshold = 100;
-    for(int i = 0; i <= iteration_threshold; i++) {
-    	    
-	lastRange = new float[rangeSampler.sampleSize()];
-	rangeSampler.fetchSample(lastRange, 0);
-	distanceValue = (int)lastRange[0];
-	
-	System.out.println("Afstand in cm: " + distanceValue);
+		Button.LEDPattern(4); // flash green led and
+		Sound.beepSequenceUp(); // make sound when ready.
+
+		Button.waitForAnyPress();
+
+		// initialiseren van de infrared sensor
+		rangeSampler = infraRed.getDistanceMode();
+		int distanceValue = 0;
+
+		final int iteration_threshold = 100;
+		for (int i = 0; i <= iteration_threshold; i++) {
+
+			lastRange = new float[rangeSampler.sampleSize()];
+			rangeSampler.fetchSample(lastRange, 0);
+			distanceValue = (int) lastRange[0];
+
+			System.out.println("Afstand in cm: " + distanceValue);
+		}
+
+		while (distanceValue > 10) {
+			motorB.setSpeed(DEFAULT_MOTOR_SPEED);
+			motorC.setSpeed(DEFAULT_MOTOR_SPEED);
+			motorB.forward();
+			motorC.forward();
+
+			if (distanceValue < 10) {
+				motorB.stop();
+				motorC.stop();
+			}
 		}
 	}
 }
-//	 range = infraRed.getDistanceMode();
-//	 float[] sample=new float[range.sampleSize()];
-//	 
-//	while () {
-//	if(rangeSampler <= 10){
-//		motorB.rotate(NINETY_DEGREES);
-//		motorC.rotate(NINETY_DEGREES);
-//		}if(rangeSampler <= 3){
-//			motorB.stop();
-//			motorC.stop();
-//			motorB.close();
-//			motorC.close();
-//		}else {
-//			motorB.forward();
-//			motorC.forward();
-//			}
-//	}
-//		Sound.beepSequence(); // we are done.
+
+// if(distanceValue <= 10){
+// motorB.rotate(NINETY_DEGREES);
+// motorC.rotate(NINETY_DEGREES);
+// }if(rangeSampler <= 3){
+// motorB.stop();
+// motorC.stop();
+// motorB.close();
+// motorC.close();
+// }else {
+// motorB.forward();
+// motorC.forward();
+// }
+// }
+// Sound.beepSequence(); // we are done.
 //
-//	}
-//	
-//}
-	
-	
-//	 private static void configureInfraredSensor(final EV3IRSensor infraredSensor, final DifferentialPilot pilot) {
-//	        final RangeFinderAdaptor rangeFinderAdaptor = new RangeFinderAdaptor(infraredSensor.getDistanceMode());
-//	        final RangeFeatureDetector rangeFeatureDetector = new RangeFeatureDetector(rangeFinderAdaptor, MAX_DISTANCE, INTERVAL);
-//	        final FeatureListener detectedObjectListener = new DetectedObjectListener(pilot);
-//	        rangeFeatureDetector.addListener(detectedObjectListener);
-//	    }
-//	
+// }
+//
+// }
 
-
+// private static void configureInfraredSensor(final EV3IRSensor infraredSensor,
+// final DifferentialPilot pilot) {
+// final RangeFinderAdaptor rangeFinderAdaptor = new
+// RangeFinderAdaptor(infraredSensor.getDistanceMode());
+// final RangeFeatureDetector rangeFeatureDetector = new
+// RangeFeatureDetector(rangeFinderAdaptor, MAX_DISTANCE, INTERVAL);
+// final FeatureListener detectedObjectListener = new
+// DetectedObjectListener(pilot);
+// rangeFeatureDetector.addListener(detectedObjectListener);
+// }
+//
 
 // Instantiate a RangeFinderAdaptor-object and tell it in which mode the sensor
 // is used
