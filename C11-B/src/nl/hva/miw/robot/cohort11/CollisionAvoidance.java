4666d7c2a26dd08b3024a2209f4c0be399fcf549
Ashley <ashleyk9@hotmail.com>
2018-03-28 12:23:44 +0200

    Collision v5

--- C11-B/src/nl/hva/miw/robot/cohort11/CollisionAvoidance.java
+++ C11-B/src/nl/hva/miw/robot/cohort11/CollisionAvoidance.java
@@ -9,7 +9,6 @@ import lejos.hardware.sensor.EV3IRSensor;
 
 import lejos.robotics.SampleProvider;
 
-
 /**
  * 
  * @author Ashley
@@ -21,8 +20,10 @@ public class CollisionAvoidance {
 		EV3IRSensor infraRed = new EV3IRSensor(SensorPort.S2); // tijdelijk op port 2
 		EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
 		EV3LargeRegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
-		final int NINETY_DEGREES = 90;
-		final int DEFAULT_MOTOR_SPEED = 100;
+		final int ONE_EIGHTY_DEGREES = 180;
+		final int DEFAULT_MOTOR_SPEED = 150;
+		final int DEFAULT_DISTANCE = 20;
+		final int MIN_DISTANCE = 4;
 		SampleProvider rangeSampler;
 		float[] lastRange;
 
@@ -46,37 +47,56 @@ public class CollisionAvoidance {
 			distanceValue = (int) lastRange[0];
 
 			System.out.println("Afstand in cm: " + distanceValue);
-			
-			if (distanceValue > 15) {
-				motorB.setSpeed(DEFAULT_MOTOR_SPEED);
-				motorC.setSpeed(DEFAULT_MOTOR_SPEED);
-				motorB.forward();
-				motorC.forward();
-				
-			}if (distanceValue <= 15) {
-				motorB.rotate(NINETY_DEGREES);
-				motorC.rotate(NINETY_DEGREES);
-				
-			}if (distanceValue < 4 || Button.ESCAPE.isUp() ) {
-				motorB.stop();
-				motorC.stop();
-				motorB.close();
-				motorC.close();
-				infraRed.close();
+
+			// rijden totdat er een obstakel gevonden wordt en dan draaien
+			switch (distanceValue) {
+			case 1:
+				while (distanceValue > DEFAULT_DISTANCE) {
+					motorB.setSpeed(DEFAULT_MOTOR_SPEED);
+					motorC.setSpeed(DEFAULT_MOTOR_SPEED);
+					motorB.forward();
+					motorC.forward();
+					break;
+				}
+			case 2:
+				if (distanceValue < DEFAULT_DISTANCE && distanceValue > MIN_DISTANCE) {
+					motorB.rotate(400);
+					motorC.rotate(-ONE_EIGHTY_DEGREES);
+					break;
+				}
+			case 3:
+				// als afstand kleiner is dan 4 centimeter, dan afsluiten
+				if (distanceValue < MIN_DISTANCE || Button.ESCAPE.isDown()) {
+					motorB.stop();
+					motorC.stop();
+					motorB.close();
+					motorC.close();
+					infraRed.close();
+					break;
+				}
 			}
 		}
-		
-
-//		while (distanceValue > 10) {
-//			motorB.setSpeed(DEFAULT_MOTOR_SPEED);
-//			motorC.setSpeed(DEFAULT_MOTOR_SPEED);
-//			motorB.forward();
-//			motorC.forward();
-//
-//			if (distanceValue < 10) {
-//				motorB.stop();
-//				motorC.stop();
-//			}
-//		}
 	}
 }
+
+// // rijden totdat er een obstakel gevonden wordt en dan draaien
+// while(distanceValue>DEFAULT_DISTANCE)
+//
+// {
+// motorB.setSpeed(DEFAULT_MOTOR_SPEED);
+// motorC.setSpeed(DEFAULT_MOTOR_SPEED);
+// motorB.forward();
+// motorC.forward();
+// break;
+// }if(distanceValue<DEFAULT_DISTANCE&&distanceValue>MIN_DISTANCE) {
+// motorB.rotate(400);
+// motorC.rotate(-ONE_EIGHTY_DEGREES);
+// }
+// // als afstand kleiner is dan 4 centimeter, dan afsluiten
+// if(distanceValue<MIN_DISTANCE||Button.ESCAPE.isDown()){
+// motorB.stop();
+// motorC.stop();
+// motorB.close();
+// motorC.close();
+// infraRed.close();
+// }
